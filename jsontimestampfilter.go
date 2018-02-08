c11d14c33d5ec9371bee98372ed0efc4fb6ce467
Gavin McNair <gavinmcnair@lease39.mcnair.co.uk>
2018-02-08 20:39:06 +0000

    Use official go formatting guidelines

--- jsontimestampfilter.go
+++ jsontimestampfilter.go
@@ -1,66 +1,66 @@
 package main
+
 import (
-	"fmt"
-	"time"
+	"bufio"
 	"encoding/json"
+	"fmt"
 	"os"
 	"strings"
-	"bufio"
-	)
+	"time"
+)
 
-const 	ISO8601 = "2006-01-02T15:04Z"
+const ISO8601 = "2006-01-02T15:04Z"
 
 type Msg struct {
-    Timestamp int64
+	Timestamp int64
 }
 
 func main() {
 
-args := os.Args[1]
+	args := os.Args[1]
 
-s := strings.Split(args, "/")
-    start, end := s[0], s[1]
+	s := strings.Split(args, "/")
+	start, end := s[0], s[1]
 
-    startTime := convertDateToMillis(start)
-    endTime := convertDateToMillis(end)
+	startTime := convertDateToMillis(start)
+	endTime := convertDateToMillis(end)
 
- fi, err := os.Stdin.Stat()
-  if err != nil {
-    panic(err)
-  }
-
-  if fi.Mode() & os.ModeNamedPipe == 0 {
-	    fmt.Println("stdin is empty")
-  } else {
-
-    scanner := bufio.NewScanner(os.Stdin)
-    for scanner.Scan() {
-	line := scanner.Text()
-	ts := getTimeStampFromLine(line)
-	if ( ts > startTime ) && ( ts < endTime ) {
-        	fmt.Println(line)
+	fi, err := os.Stdin.Stat()
+	if err != nil {
+		panic(err)
 	}
-    }
 
-    if err := scanner.Err(); err != nil {
-        fmt.Println("Error")
-    }
+	if fi.Mode()&os.ModeNamedPipe == 0 {
+		fmt.Println("stdin is empty")
+	} else {
 
-  } 
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			line := scanner.Text()
+			ts := getTimeStampFromLine(line)
+			if (ts > startTime) && (ts < endTime) {
+				fmt.Println(line)
+			}
+		}
+
+		if err := scanner.Err(); err != nil {
+			fmt.Println("Error")
+		}
+
+	}
 
 }
 
 func convertDateToMillis(date string) int64 {
-   var formattedTime  time.Time
-   formattedTime, _ = time.Parse(ISO8601, date)
+	var formattedTime time.Time
+	formattedTime, _ = time.Parse(ISO8601, date)
 
-   nanotime := formattedTime.UnixNano()
-   return nanotime / 1000000
+	nanotime := formattedTime.UnixNano()
+	return nanotime / 1000000
 }
 
 func getTimeStampFromLine(jsonInput string) int64 {
-    var msg Msg
-    json.Unmarshal([]byte(jsonInput), &msg)
-    return msg.Timestamp
+	var msg Msg
+	json.Unmarshal([]byte(jsonInput), &msg)
+	return msg.Timestamp
 }
-
--- jsontimestampfilter_test.go
+++ jsontimestampfilter_test.go
@@ -1,33 +1,26 @@
 package main
 
-import (
-	"testing"
-	)
-
+import "testing"
 
 const (
-TEST_DATE="2006-01-02T15:04Z"
-TEST_EPOCH=1136214240000
-JSON_LINE=`{"timestamp":1511290852857,"animal":"rabbit","car":"mustang","fruit":"apple" }`
-JSON_EPOCH=1511290852857
+	TEST_DATE  = "2006-01-02T15:04Z"
+	TEST_EPOCH = 1136214240000
+	JSON_LINE  = `{"timestamp":1511290852857,"animal":"rabbit","car":"mustang","fruit":"apple" }`
+	JSON_EPOCH = 1511290852857
 )
 
 func TestConvertDateToMillis(t *testing.T) {
+	time := convertDateToMillis(TEST_DATE)
 
-   time := convertDateToMillis(TEST_DATE)
-
-   if time != TEST_EPOCH {
-       t.Errorf("Epoch Time was wrong, got: %d, want: %d.", time, TEST_EPOCH)
-    }
-
+	if time != TEST_EPOCH {
+		t.Errorf("Epoch Time was wrong, got: %d, want: %d.", time, TEST_EPOCH)
+	}
 }
 
 func TestGetTimeStampFromLine(t *testing.T) {
+	timestamp := getTimeStampFromLine(JSON_LINE)
 
-  timestamp := getTimeStampFromLine(JSON_LINE)
-
-     if timestamp != JSON_EPOCH {
-       t.Errorf("Epoch Time was wrong, got: %d, want: %d.", timestamp, JSON_EPOCH)
-    }
-
+	if timestamp != JSON_EPOCH {
+		t.Errorf("Epoch Time was wrong, got: %d, want: %d.", timestamp, JSON_EPOCH)
+	}
 }
